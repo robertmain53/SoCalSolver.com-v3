26b12db5828ed26eab72627a8819164f2975817d
mainrobert53 <mainrobert53@gmail.com>
2025-06-26 10:52:40 +0200

    ai generated fixes 15

--- frontend/plugins/ssr-log.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  if (process.server) {
-    const { locale } = useI18n()
-    const route = useRoute()
-    // eslint-disable-next-line no-console
-    console.log('[NUXT SSR] locale:', locale.value, 'route:', route.fullPath)
-  }
-})
